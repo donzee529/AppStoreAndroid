3a6d4c78e61be6e0366dffb88e7de0f52792f790
Ashi Vered <76535872+AshiVered@users.noreply.github.com>
2022-10-23 01:23:24 +0300

    create

---
+++ README.md
@@ -0,0 +1,13 @@
+based on https://github.com/bishwassagar/Android-Webview-App
+# Android-Root-Market
+AppMarket for Android. the app installing apps via shell comands.(need root).
+# Chnage URL
+in line 85.
+        mWebView.loadUrl("https://yourdomain.com"); //Replace The Link Here
+# note
+The link to download an app through the market should look like this:
+yourdomain.com/yourapp.apk
+So that the market will recognize the name of the downloaded file and you can install it.
+
+
+
---
+++ app/src/main/java/com/webview/myapplication/DownloadBroadcastReceiver.java
@@ -0,0 +1,39 @@
+package com.webview.myapplication;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class DownloadBroadcastReceiver extends BroadcastReceiver {
+    private WebView mWebView;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+            //install app
+            try {
+                Process su = Runtime.getRuntime().exec("su");
+                DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+
+                outputStream.writeBytes("su -c mv /sdcard/download/"+ MainActivity.appName     +" /data/local/tmp/file.apk && su -c pm install /data/local/tmp/file.apk && su -c rm /data/local/tmp/file.apk");
+                outputStream.flush();
+
+                outputStream.writeBytes("exit\n");
+                outputStream.flush();
+                su.waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
